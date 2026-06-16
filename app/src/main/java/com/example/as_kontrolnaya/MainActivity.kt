ecc3a53df51bfa8fb73c451b8fad6bd9293bd43c
iguar63 <gdlgjhlkjklj@gmail.com>
2026-06-16 11:23:50 +0300

    .

--- app/src/main/java/com/example/as_kontrolnaya/MainActivity.kt
+++ app/src/main/java/com/example/as_kontrolnaya/MainActivity.kt
@@ -25,21 +25,21 @@ class MainActivity : AppCompatActivity() {
     private fun calculate() = with(binding) {
         try {
             val bill = totalAmount.text.toString().toDouble()
-            val tipsPercent = if (tipsPercent.text.toString().isEmpty()) 15 else tipsPercent.text.toString().toInt()
-            val people = if (peopleAmount.text.toString().isEmpty()) 1 else peopleAmount.text.toString().toInt()
+            val tipsPercent = tipsPercent.text.toString().toInt()
+            val people = peopleAmount.text.toString().toInt()
 
             if (bill <= 0) {
-                Toast.makeText(this@MainActivity, "ОШИБКА!! Укажите корректную сумму", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "ОШИБКА!! Укажите корректную сумму", Toast.LENGTH_LONG).show()
                 return
             }
 
-            if (bill <= 0 || tipsPercent < 5 || tipsPercent > 30) {
-                Toast.makeText(this@MainActivity, "ОШИБКА!! Укажите корректные чаевые", Toast.LENGTH_SHORT).show()
+            if (tipsPercent <= 0 || tipsPercent < 5 || tipsPercent > 30) {
+                Toast.makeText(this@MainActivity, "ОШИБКА!! Укажите корректные чаевые", Toast.LENGTH_LONG).show()
                 return
             }
 
             if (people < 1 || people > 20) {
-                Toast.makeText(this@MainActivity, "ОШИБКА!! Укажите корректное число людей", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, "ОШИБКА!! Укажите корректное число людей", Toast.LENGTH_LONG).show()
                 return
             }
 
@@ -72,7 +72,7 @@ class MainActivity : AppCompatActivity() {
             hiddenSum.visibility = android.view.View.VISIBLE
 
         } catch(e: Exception) {
-            Toast.makeText(this@MainActivity, "Ошибка", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity, "Ошибка", Toast.LENGTH_LONG).show()
         }
     }
 }
